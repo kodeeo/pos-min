127e0403c73f5e7da37d7c7b82e5db55fa42a077
meXuvo <cse.engr.shuvo@gmail.com>
2020-10-01 14:47:53 +0600

    some responsive issu fixed

--- resources/views/admin/supplier/index.blade.php
+++ resources/views/admin/supplier/index.blade.php
@@ -38,17 +38,17 @@
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body">
-                                <table id="example1" class="table table-bordered table-striped text-center table-responsive">
+                                <table id="example1" class="table table-bordered table-striped text-center table-responsive-xl">
                                     <thead>
                                     <tr>
                                         <th>Serial</th>
                                         <th>Name</th>
-                                        <th>Image</th>
+                                        <th class="d-lg-table-cell d-none sorting">Image</th>
                                         <th>Email</th>
                                         <th>Phone</th>
                                         <th>Address</th>
-                                        <th>City</th>
-                                        <th>Type</th>
+                                        <th class="d-lg-table-cell d-none sorting">City</th>
+                                        <th class="d-lg-table-cell d-none sorting">Type</th>
                                         <th>Actions</th>
                                     </tr>
                                     </thead>
@@ -56,12 +56,12 @@
                                     <tr>
                                         <th>Serial</th>
                                         <th>Name</th>
-                                        <th>Image</th>
+                                        <th class="d-lg-table-cell d-none" >Image</th>
                                         <th>Email</th>
                                         <th>Phone</th>
                                         <th>Address</th>
-                                        <th>City</th>
-                                        <th>Type</th>
+                                        <th class="d-lg-table-cell d-none">City</th>
+                                        <th class="d-lg-table-cell d-none">Type</th>
                                         <th>Actions</th>
                                     </tr>
                                     </tfoot>
